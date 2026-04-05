361acaf5fa506748acb4811d5545d75e4d783ff2
BensCode <ruben.simarmata2@gmail.com>
2026-04-06 06:12:52 +0700

    class DataMahasiswa103082400008

---
+++ tpmodul7_kelompok_5/dataMahasiswa103082400008.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_5
+{
+    internal class dataMahasiswa103082400008
+    {
+        public class Nama
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+
+        }
+        public class Mahasiswa
+        {
+            public Nama nama { get; set; }
+            public long nim { get; set; }
+            public string fakultas { get; set; }
+
+        }
+        public void ReadJSON()
+        {
+            string filePath = "tpModul7_1_103082400008.json";
+            string jsonString = File.ReadAllText(filePath);
+
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} denghan nim {mhs.nim} fakultas {mhs.fakultas}");
+        }
+    }
+}
